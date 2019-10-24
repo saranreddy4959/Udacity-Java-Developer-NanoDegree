c6fac27731b846c3bdeb5ce50863cd839d3c27bb
saran kumar reddy padala <saranreddy4959@gmail.com>
2019-10-24 17:49:33 -0500

    Spring Boot Application using Thymeleaf

--- 3.Spring_Boot_Template_Engine/spring-boot-thymeleaf/src/main/java/com/example/springbootthymeleaf/controller/UserController.java
+++ 3.Spring_Boot_Template_Engine/spring-boot-thymeleaf/src/main/java/com/example/springbootthymeleaf/controller/UserController.java
@@ -11,8 +11,28 @@ public class UserController {
     @RequestMapping("demo")
     public String demo(Model model){
         model.addAttribute("message","Hello Thymeleaf");
+        double grade = 90.5;
+        model.addAttribute("grade",grade);
+        model.addAttribute("gpa",convertGPA(grade));
+        return "demo";
+    }
 
-        return "demo.html";
+    public String convertGPA(double grade){
+        if (grade>=90){
+            return "A";
+        }
+        else if (grade<90 && grade>=80){
+            return "B";
+        }
+        else if (grade<80 && grade>=70){
+            return "C";
+        }
+        else if(grade<70 && grade>=60){
+            return "D";
+        }
+        else{
+            return "F";
+        }
     }
 
 
