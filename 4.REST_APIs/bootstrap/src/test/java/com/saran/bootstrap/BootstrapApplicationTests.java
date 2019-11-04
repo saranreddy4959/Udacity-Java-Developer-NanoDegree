436cf24aad350fe57b5d42a84e1550b897711423
saran kumar reddy padala <saranreddy4959@gmail.com>
2019-11-03 23:15:07 -0600

    Developed an API to return a list of locations

---
+++ 4.REST_APIs/bootstrap/src/test/java/com/saran/bootstrap/BootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saran.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
