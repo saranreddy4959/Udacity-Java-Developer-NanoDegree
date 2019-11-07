0c32c565c0581098e9afeeae18b20e09485fa7a4
saran kumar reddy padala <saranreddy4959@gmail.com>
2019-11-07 17:59:40 -0600

    graphql for dogs data

---
+++ 4.REST_APIs/dogs_graphql/src/test/java/com/saran/dogs_graphql/DogsGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saran.dogs_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
