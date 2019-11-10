fd2cf2c88b634217428e3bb1cbfb3f08a3e99390
saran kumar reddy padala <saranreddy4959@gmail.com>
2019-11-09 22:07:36 -0600

    dogs data microservice

---
+++ 4.REST_APIs/microservice-dog-data/src/main/java/com/saran/microservicedogdata/MicroserviceDogDataApplication.java
@@ -0,0 +1,13 @@
+package com.saran.microservicedogdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDogDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDogDataApplication.class, args);
+	}
+
+}
---
+++ 4.REST_APIs/microservice-dog-data/src/test/java/com/saran/microservicedogdata/MicroserviceDogDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saran.microservicedogdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDogDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
