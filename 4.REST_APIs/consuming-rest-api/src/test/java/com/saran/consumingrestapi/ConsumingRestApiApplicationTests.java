fbd2597d3742beb2cc49259155ad1fb62ac8b65f
saran kumar reddy padala <saranreddy4959@gmail.com>
2019-11-12 08:16:22 -0600

    Extracting an rest api details from the url

---
+++ 4.REST_APIs/consuming-rest-api/src/test/java/com/saran/consumingrestapi/ConsumingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saran.consumingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
